f40dd297eefcadee547c463488fdbb6af81d0d99
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-04-14 16:38:57 -0700

    workout schema

---
+++ models/workout.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter a type of exercise",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter a name of the exercise",
+      },
+      duration: {
+        type: Number,
+        required: "Enter the duration of the exercise in minutes",
+      },
+      distance: {
+        type: Number,
+      },
+      Weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+module.exports.model("Workout", WorkoutSchema);
