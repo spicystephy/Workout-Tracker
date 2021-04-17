d30666c6e779a6dc796275b834641b76bc1f8d5f
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-04-17 14:42:39 -0700

    fixed api routes

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-// const { Workout } = require("../models");
+const { Workout } = require("../models");
 
 // this is the full route --> /api/workouts...
 router.get("/workouts", async function (req, res) {
@@ -21,7 +21,7 @@ router.get("/workouts", async function (req, res) {
 
 router.put("/workouts/:id", async function (req, res) {
   try {
-    const updateWorkout = await Workout.findByIDAndUpdate(
+    const updateWorkout = await Workout.findByIdAndUpdate(
       req.params.id,
       {
         $push: { exercises: req.body },
@@ -36,7 +36,8 @@ router.put("/workouts/:id", async function (req, res) {
 
 router.post("/workouts", async function (req, res) {
   try {
-    const newWorkout = await Workout.create({});
+    const newWorkout = await Workout.create(req.body);
+    console.log(newWorkout);
     res.json(newWorkout);
   } catch (err) {
     res.status(500).json(err);
@@ -45,19 +46,14 @@ router.post("/workouts", async function (req, res) {
 
 router.get("/workouts/range", async function (req, res) {
   try {
-    const range = await Workout.find({})
-    .sort({ day: -1 })
-    .limit(7)
-    .then((range) => {
-      res.json(range);
-    })
+    const range = await Workout.find({}).sort({ day: -1 }).limit(7);
+    res.json(range);
   } catch (err) {
-    res.status(500).send();
+    res.status(500).json(err);
   }
 });
 
 module.exports = router;
-
 
 // router.get("/workouts", async function (req, res) {
 //   try {
